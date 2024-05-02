ad5fe0e0cd3e6cce6bee7c32804db2a17ea745b1
jae yeong <chujy1224@gmail.com>
2024-05-03 01:46:35 +0900

    김영한의 자바 입문 - 변수

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // 주석... 넵
+
+        /*
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+         */
+    }
+}
---
+++ java-start/src/variable/Var6.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        // 변수는 초기화를 해야한다. (지역변수)
+        // 초기화 하지 않고 사용할라면 컴파일 에러 발생 (java: variable a might not have been initialized)
+        int a;
+//        System.out.println(a);
+    }
+}
